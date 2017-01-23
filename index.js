b05a0cf5dc2250db465d659cd3d4addcd8d5e609
Tanapol Nearunchorn <tanapoln@wongnai.com>
2017-01-23 23:12:25 +0700

    fix code style && add comment

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
-    let express = require('express')
-    let ua = require('universal-analytics')
+let express = require('express')
+let ua = require('universal-analytics')
 
-    let app = express()
-    let visitor = ua('UA-34321454-1', 'user1', {strictCidFormat: false, https: false});
+let app = express()
+let visitor = ua('UA-34321454-1', 'user1', {strictCidFormat: false, https: false});
 
-    app.get('/fast', function(req ,res) {
-        res.send('OK')
-    })
+app.get('/fast', function(req ,res) {
+    res.send('OK')
+})
 
-    app.get('/slow', function(req, res) {
-        visitor.pageview('/slow').send()
-        res.send('OK')
-    })
+app.get('/slow', function(req, res) {
+    //This line will simply fire HTTP request to Google Analytics
+    visitor.pageview('/slow').send()
+    res.send('OK')
+})
 
-    app.listen(3000, function() {
-        console.log("Server started at port 3000")
-    })
+app.listen(3000, function() {
+    console.log("Server started at port 3000")
+})
